fb9f84975568a178cefb89d7d102af9e4661cffd
Leyal <305551249@qq.com>
2018-05-24 20:59:54 +0800

    Initial Commit

---
+++ EatChicken/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EatChicken
+//
+//  Created by 雷源 on 2018/5/24.
+//  Copyright © 2018年 Yuanjihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EatChicken/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EatChicken
+//
+//  Created by 雷源 on 2018/5/24.
+//  Copyright © 2018年 Yuanjihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
